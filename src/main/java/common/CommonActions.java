1a50dbd80c5eb7a196262c1fc3df97cfd362a5b1
Lera Roy <valeria98.val@gmail.com>
2022-10-31 15:20:53 +0200

    init

--- src/main/java/common/CommonActions.java
+++ src/main/java/common/CommonActions.java
@@ -3,6 +3,8 @@ package common;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
@@ -17,13 +19,22 @@ public class CommonActions {
 
         switch (BROWSER){
             case "chrome":
-                //WebDriverManager.chromedriver().setup();
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+                WebDriverManager.chromedriver().setup();
                 driver=new ChromeDriver();
                 break;
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 driver=new FirefoxDriver();
+                break;
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                driver=new EdgeDriver();
+                break;
+            case "chromeHeadless":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options=new ChromeOptions();
+                options.addArguments("--headless");
+                driver=new ChromeDriver(options);
                 break;
             default:
                 System.out.println("Incorrect browser name"+BROWSER);
